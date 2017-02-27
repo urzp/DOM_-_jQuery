a94a53f43c7c2cae63edc17fd9cd30d7a27ce4f2
urzp <urzp002@mail.ru>
2017-02-27 22:00:29 +0300

    js ready for navigetion

--- js/script.js
+++ js/script.js
@@ -1,18 +1,60 @@
+
+
+    function clear_alle(){
+        $('#container').children().remove()
+    };
+
+    function clear_main(){
+        $('.mane').children().remove()
+    };
+
+    function set_stds_el(){
+        $("#container").append("<div class='header'></div>");
+        $("#container").append("<div class='mane'></div>");
+        $("#container").append("<div class='footer'></div>");
+        $(".header").append("<nav>");
+        $("nav").append("<ul></ul>");
+        $("nav ul").append("<li id='logo'><img src='img/logo.gif' alt='logo' style='width: 300px;'></li>");
+        $("nav ul").append("<li><a href='#'>About US</a></li>");
+        $("nav ul").append('<li><a href="#">our menu</a></li>');
+        $("nav ul").append('<li><a href="#">news</a></li>');
+        $("nav ul").append('<li><a href="#">contacts</a></li>');
+    };
+
+    function index_page(){
+        clear_main();
+        $(".mane").append("<h1>Welcome to us<h1>");
+        $(".mane").append(' <img src="img/main.jpg" alt="restoran" > ');  
+    };
+    
+     function about_us_page(){
+        clear_main();
+    };
+
+     function our_meny_page(){
+        clear_main();
+    };
+
+     function news_page_page(){
+        clear_main();
+    };
+
+     function contacts_page(){
+        clear_main();
+    };
+
+
+
 $(function(){
+    set_stds_el();
+    index_page(); 
     
-    $("#container").append("<div class='header'></div>");
-    $("#container").append("<div class='mane'></div>");
-    $("#container").append("<div class='footer'></div>");
-    $(".header").append("<nav>");
-    $("nav").append("<ul></ul>");
-    $("nav ul").append("<li id='logo'><img src='http://www.logowiks.com/wp-content/uploads/2017/01/best-restaurant-logo-design-7.gif' alt='logo' style='width: 300px;'></li>");
-    $("nav ul").append("<li><a href='#'>About US</a></li>");
-    $("nav ul").append('<li><a href="#">our meny</a></li>');
-    $("nav ul").append('<li><a href="#">news</a></li>');
-    $("nav ul").append('<li><a href="#">contacts</a></li>');
-    $(".header").append("<h1>Welcome to us<h1>");
-    $(".mane").append(' <img src="http://helphome.ru/wp-content/uploads/2014/08/gusto-02.jpg" alt="restoran" > ');
-    
+    $('nav > ul > li:nth-child(1)').click( function(){index_page()});
+    $('nav > ul > li:nth-child(2)').click( function(){about_us_page()});
+    $('nav > ul > li:nth-child(3)').click( function(){our_meny_page()});
+    $('nav > ul > li:nth-child(4)').click( function(){news_page_page()});
+    $('nav > ul > li:nth-child(5)').click( function(){news_page_page()});
+
 });
 
 
